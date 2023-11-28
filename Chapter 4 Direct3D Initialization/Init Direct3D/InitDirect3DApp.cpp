17b0a8f705633232078c354b016fca9560b3f32c
zhangjiaqi12 <zhangjiaqi12@corp.netease.com>
2023-11-28 19:01:53 +0800

    # cpp mod

--- Chapter 4 Direct3D Initialization/Init Direct3D/InitDirect3DApp.cpp
+++ Chapter 4 Direct3D Initialization/Init Direct3D/InitDirect3DApp.cpp
@@ -1,4 +1,4 @@
-//***************************************************************************************
+﻿//***************************************************************************************
 // Init Direct3D.cpp by Frank Luna (C) 2015 All Rights Reserved.
 //
 // Demonstrates the sample framework by initializing Direct3D, clearing 
@@ -14,10 +14,10 @@ using namespace DirectX;
 class InitDirect3DApp : public D3DApp
 {
 public:
-	InitDirect3DApp(HINSTANCE hInstance);
-	~InitDirect3DApp();
+    InitDirect3DApp(HINSTANCE hInstance);
+    ~InitDirect3DApp();
 
-	virtual bool Initialize()override;
+    virtual bool Initialize()override;
 
 private:
     virtual void OnResize()override;
@@ -27,22 +27,22 @@ private:
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
-				   PSTR cmdLine, int showCmd)
+    PSTR cmdLine, int showCmd)
 {
-	// Enable run-time memory check for debug builds.
+    // Enable run-time memory check for debug builds. 用于运行时内存检查，开启debug模式
 #if defined(DEBUG) | defined(_DEBUG)
-	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
     try
     {
         InitDirect3DApp theApp(hInstance);
-        if(!theApp.Initialize())
+        if (!theApp.Initialize())
             return 0;
 
         return theApp.Run();
     }
-    catch(DxException& e)
+    catch (DxException& e)
     {
         MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
         return 0;
@@ -50,7 +50,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 }
 
 InitDirect3DApp::InitDirect3DApp(HINSTANCE hInstance)
-: D3DApp(hInstance) 
+    : D3DApp(hInstance)
 {
 }
 
@@ -60,15 +60,15 @@ InitDirect3DApp::~InitDirect3DApp()
 
 bool InitDirect3DApp::Initialize()
 {
-    if(!D3DApp::Initialize())
-		return false;
-		
-	return true;
+    if (!D3DApp::Initialize())
+        return false;
+
+    return true;
 }
 
 void InitDirect3DApp::OnResize()
 {
-	D3DApp::OnResize();
+    D3DApp::OnResize();
 }
 
 void InitDirect3DApp::Update(const GameTimer& gt)
@@ -78,46 +78,45 @@ void InitDirect3DApp::Update(const GameTimer& gt)
 
 void InitDirect3DApp::Draw(const GameTimer& gt)
 {
-    // Reuse the memory associated with command recording.
-    // We can only reset when the associated command lists have finished execution on the GPU.
-	ThrowIfFailed(mDirectCmdListAlloc->Reset());
+    // 重用 记录指令相关的内存
+    // 只有当与GPU关联的命令列表执行完成是，才可以重置
+    ThrowIfFailed(mDirectCmdListAlloc->Reset());
 
-	// A command list can be reset after it has been added to the command queue via ExecuteCommandList.
-    // Reusing the command list reuses memory.
+    // ExecuteCommandList 将某个命令加入队列之后，我们可以重置list，复用list的内存
     ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), nullptr));
 
-	// Indicate a state transition on the resource usage.
-	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(),
-		D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET));
+    // 资源状态转换，从present转换到 rt
+    mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(),
+        D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET));
 
-    // Set the viewport and scissor rect.  This needs to be reset whenever the command list is reset.
+    // 设置窗口和剪裁矩形
     mCommandList->RSSetViewports(1, &mScreenViewport);
     mCommandList->RSSetScissorRects(1, &mScissorRect);
 
-    // Clear the back buffer and depth buffer.
-	mCommandList->ClearRenderTargetView(CurrentBackBufferView(), Colors::LightSteelBlue, 0, nullptr);
-	mCommandList->ClearDepthStencilView(DepthStencilView(), D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.0f, 0, 0, nullptr);
-	
-    // Specify the buffers we are going to render to.
-	mCommandList->OMSetRenderTargets(1, &CurrentBackBufferView(), true, &DepthStencilView());
-	
-    // Indicate a state transition on the resource usage.
-	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(),
-		D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
+    // 清除后台缓冲区和深度缓冲区的内容
+    mCommandList->ClearRenderTargetView(CurrentBackBufferView(), Colors::LightSteelBlue, 0, nullptr);
+    mCommandList->ClearDepthStencilView(DepthStencilView(), D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.0f, 0, 0, nullptr);
 
-    // Done recording commands.
-	ThrowIfFailed(mCommandList->Close());
- 
-    // Add the command list to the queue for execution.
-	ID3D12CommandList* cmdsLists[] = { mCommandList.Get() };
-	mCommandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
-	
-	// swap the back and front buffers
-	ThrowIfFailed(mSwapChain->Present(0, 0));
-	mCurrBackBuffer = (mCurrBackBuffer + 1) % SwapChainBufferCount;
+    // 自定渲染的缓冲区
+    mCommandList->OMSetRenderTargets(1, &CurrentBackBufferView(), true, &DepthStencilView());
 
-	// Wait until frame commands are complete.  This waiting is inefficient and is
-	// done for simplicity.  Later we will show how to organize our rendering code
-	// so we do not have to wait per frame.
-	FlushCommandQueue();
+    // 资源状态转换，从RT到present
+    mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(),
+        D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
+
+    // 完成渲染指令的记录
+    ThrowIfFailed(mCommandList->Close());
+
+    // 将待执行List 提交到 queue上
+    ID3D12CommandList* cmdsLists[] = { mCommandList.Get() };
+    mCommandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
+
+    // 交换缓冲区进行呈现
+    ThrowIfFailed(mSwapChain->Present(0, 0));
+    mCurrBackBuffer = (mCurrBackBuffer + 1) % SwapChainBufferCount;
+
+    // Wait until frame commands are complete.  This waiting is inefficient and is
+    // done for simplicity.  Later we will show how to organize our rendering code
+    // so we do not have to wait per frame.
+    FlushCommandQueue();
 }
